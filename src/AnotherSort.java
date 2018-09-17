d3ba29030f38e7a9fb4b20483ce080c70a1dad0d
Stiv Kapten <stkapt@ttu.ee>
2018-09-17 15:58:44 +0300

    Added new sorting another sorting algo

--- src/AnotherSort.java
+++ src/AnotherSort.java
@@ -8,40 +8,7 @@ import java.util.*;
  * @since 28/11/2016.
  */
 public class GettingSorted {
-    /**
-     * This method conveys my first effort towards a bubble sorting
-     * algorithm. The array in itself is sorted
-     * (no auxiliary array is used) by overwriting adjacent values. First
-     * one value is stored in the variable 'c' (which is a 'vahemuutuja'
-     * and is short for 'copy'). Then this corresponding array element is
-     * overwritten and so is the other array element overwritten to the
-     * value of c.
-     * Boolean s is used a a switch. Determined to assign the array another
-     * lookover until no changes have been made, in which case no changes
-     * are to be made.
-     *
-     * @param unsorted
-     * @param sorted
-     * @return
-     */
 
-    int[] getSortedByBubbleArray(int[] unsorted) {
-        int c = 0;
-        boolean s = false;
-        for (int i = 1; i < unsorted.length; i++ ) {
-            if (unsorted[i - 1] > unsorted[i]) {
-                s = true;
-                c = unsorted[i];  // vahemällu
-                unsorted[i] = unsorted[i - 1];
-                unsorted[i - 1] = c;
-            }
-            if (i == unsorted.length - 1 && s == true) {
-                i = 0; // cursor brought back to 0
-                s = false;
-            }
-        }
-        return unsorted;
-    }
 
     int[] AnotherSort (int[] unsorted) {
         long tStart = System.currentTimeMillis();
@@ -86,15 +53,6 @@ public class GettingSorted {
         System.out.println("This is bubble sorting according to AnotherSort:");
         System.out.println(Arrays.toString(unsorted));
         System.out.println("Time elapsed " + elapsedSeconds + " seconds.");
-        return unsorted;
-    }
-
-    int[] getRandomArray(int arrayLength, int integerBound) {
-        int[] unsorted = new int[arrayLength];
-        Random rand = new Random();
-        for( int i = 0; i < unsorted.length; i++ ) {
-            unsorted[i] = rand.nextInt(integerBound);
-        }
         return unsorted;
     }
 }
